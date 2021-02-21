d2fce766e89979db39f68f9b6c344c880bbd9765
jdcar <jdcarlstone@gmail.com>
2021-02-21 14:09:11 -0600

    fix jquery error in console log

--- public/js/favorites.js
+++ public/js/favorites.js
@@ -1,33 +1,33 @@
 $(document).ready(() => {
     console.log("Loading page")
     // all the documents being added
-    const jobTitle = $("input#form-title");
-    const company = $("input#form-company");
-    const location = $("input#form-location");
-    const snippit = $("input#form-snippit");
-    const link = $("input#form-link");
-    const deadline = $("input#form-deadline");
-    const notes = $("input#form-notes");
-    const interest = $("#form-interest");
-    const status = $("#form-status");
+    const jobTitle = $("input#form-title").val();
+    const company = $("input#form-company").val();
+    const location = $("input#form-location").val();
+    const snippit = $("input#form-snippit").val();
+    const link = $("input#form-link").val();
+    const deadline = $("input#form-deadline").val();
+    const notes = $("input#form-notes").val();
+    const interest = $("#form-interest").val();
+    const status = $("#form-status").val();
     const submit = $("#submit");
 
 
     submit.on("click", e => {
         e.preventDefault();
 
-        console.log(jobTitle.val())
+        // console.log(jobTitle.val())
 
         const jobInput = {
-            jobtitle: jobTitle.val(),
-            company: company.val(),
-            location: location.val(),
-            snippit: snippit.val(),
-            link: link.val(),
-            deadline: deadline.val(),
-            notes: notes.val(),
-            interest: interest.val(),
-            status: status.val(),
+            jobtitle: jobTitle,
+            company: company,
+            location: location,
+            snippit: snippit,
+            link: link,
+            deadline: deadline,
+            notes: notes,
+            interest: interest,
+            status: status,
         }
         // submitForm(jobInput.val())
         console.log(jobInput)
