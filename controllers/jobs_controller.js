8a4107c84a1866cf595fcab8be39df9ea183ae48
jdcar <jdcarlstone@gmail.com>
2021-02-22 15:27:02 -0600

    Renders database data on home page

--- controllers/jobs_controller.js
+++ controllers/jobs_controller.js
@@ -1,16 +1,23 @@
 // const { decodeBase64 } = require('bcryptjs');
 var express = require('express')
 var router = express.Router();
-// var jobs = require('../models/jobs')
+var jobs = require('../models/jobs')
 var db = require("../models");
 
 console.log("hello world")
 
 
 router.get("/home", function (req, res) {
+
+    db.Jobs.findAll({}).then(function (dbJobs) {
+
+        // dbJobs = JSON.stringify(dbJobs)
+
+        res.render('home', { Jobs: dbJobs })
+    })
     
-    var test = { data: "data"}
-    res.render('home', {test})
+    // var test = { data: "data"}
+    // res.render('home', {test})
 })
 
 router.post("/api/favorites" , function (req, res) {
