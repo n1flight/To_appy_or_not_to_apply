bb1440f7453f57c8530ed6437776ef1233132993
N1flight <nadersalem123@gmail.com>
2021-02-27 13:55:30 -0600

    force true removed

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ const routes = require("./controllers/jobs_controller.js");
 app.use(routes)
 
 // Syncing our database and logging a message to the user upon success
-db.sequelize.sync({force: true}).then(() => {
+db.sequelize.sync().then(() => {
   app.listen(PORT, () => {
     console.log(
       `==> 🌎  Listening on port %s. Visit http://localhost:${PORT} in your browser.`,
